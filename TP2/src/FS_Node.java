88ad2150ab6abe388b1d091b02aed54cf07ae0db
150benjamim <benjamimrodrigues44@gmail.com>
2023-12-17 20:13:12 +0000

    test

--- TP2/src/FS_Node.java
+++ TP2/src/FS_Node.java
@@ -617,6 +617,9 @@ public class FS_Node {
 
                     byte[] dataHashBytes = digest.digest();
 
+                    System.out.println(Arrays.toString(blocksToReceive.get(blockToReceive)));
+
+                    System.out.println(Arrays.toString(dataHashBytes));
 
                     if (Arrays.equals(blocksToReceive.get(blockToReceive), dataHashBytes)) {
                         blocksToReceive.remove(blockToReceive);
